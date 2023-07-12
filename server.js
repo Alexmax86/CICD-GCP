16ec9046c4a143c9278d07829d07392f6149c332
Alexmax86 <alessio.mazzei86@gmail.com>
2023-07-12 17:11:59 +0100

    Test for CI/CD 6

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World. This is an update to the commit');
+  res.send('Hello World. This is the new file');
 });
 
 app.listen(PORT, HOST, () => {
