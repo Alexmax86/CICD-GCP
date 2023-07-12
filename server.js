4ab1ecf10db0ac4c16d3eabfa4368ae7ee98213e
Alexmax86 <alessio.mazzei86@gmail.com>
2023-07-12 18:15:14 +0100

    Test for CI/CD 9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World. This is an automatic restart');
+  res.send('Hello World. This is Continuous Deployement example');
 });
 
 app.listen(PORT, HOST, () => {
