708d8f6820dd542d38cf0241f9a836f3c2b52ba7
Alexmax86 <alessio.mazzei86@gmail.com>
2023-07-16 10:25:02 +0100

    Test for CI/CD: Metadata test 1

---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+
+
+// App
+const app = express();
+
+// App config
+app.set('view engine', 'ejs');
+
+app.get('/home', (req, res) => {
+  
+});
+
+app.get('/', (req, res) => {
+  const myVariable = 'Red Rabbit';
+  res.render('home', { variableName: myVariable });  
+});
+
+const mysql = require('mysql');
+
+//DB connection
+/*
+const connection = mysql.createConnection({
+  host: 'your_database_host',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  }
+  console.log('Connected to the database!');
+*/
+
+
+// Listen command 
+ 
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
